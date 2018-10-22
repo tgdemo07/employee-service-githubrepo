c9a845d0b9844375c196005dd0003d0af8a0ef60
Tommy Gundersen <togunder@microsoft.com>
2018-10-22 20:37:27 +0200

    fixed missing methods

--- src/main/java/payroll/employeeservice/Employee.java
+++ src/main/java/payroll/employeeservice/Employee.java
@@ -18,4 +18,28 @@ class Employee {
 		this.name = name;
 		this.role = role;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id){
+		this.id = id;
+	}
 }
