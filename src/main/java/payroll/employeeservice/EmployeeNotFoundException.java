38e5d2710f471755397efea7a0b6aca217e8b555
Tommy Gundersen <togunder@microsoft.com>
2018-10-22 20:50:09 +0200

    commented on exception

--- src/main/java/payroll/employeeservice/EmployeeNotFoundException.java
+++ src/main/java/payroll/employeeservice/EmployeeNotFoundException.java
@@ -4,6 +4,7 @@ package payroll.employeeservice;
 class EmployeeNotFoundException extends RuntimeException {
 
 	EmployeeNotFoundException(Long id) {
+		//
 		super("Could not find employee " + id);
 	}
 }
