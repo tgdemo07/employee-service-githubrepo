3d85d50078488f8d901f1c41b008f8935248ecfc
tgdemo07 <44376007+tgdemo07@users.noreply.github.com>
2018-10-23 08:31:25 +0200

    Created third generation roles

--- src/main/java/payroll/employeeservice/LoadDatabase.java
+++ src/main/java/payroll/employeeservice/LoadDatabase.java
@@ -13,8 +13,8 @@ class LoadDatabase {
 	@Bean
 	CommandLineRunner initDatabase(EmployeeRepository repository) {
 		return args -> {
-			repository.save(new Employee("Bilbo Baggins", "burglar2"));
-			repository.save(new Employee("Frodo Baggins", "thief2"));
+			repository.save(new Employee("Bilbo Baggins", "burglar3"));
+			repository.save(new Employee("Frodo Baggins", "thief3"));
 		};
 	}
 }
